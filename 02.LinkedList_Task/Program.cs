5ab3c59280a9629c20555d9af79d35f416886390
MongLick <qkjm4836@gmail.com>
2024-04-26 19:26:07 +0900

    feat : 연결리스트

---
+++ 02.LinkedList_Task/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _02.LinkedList_Task
+{
+	internal class Program
+	{
+		// 사용자의 입력으로 숫자를 입력 받아서(마이너스도 허용)
+		// 음수는 앞에 추가하고, 양수는 뒤에 추가하여
+		// 음수&양수를 반으로 나누는 연결리스트 구현
+		// 입력 받을 때마다 처음부터 끝까지 출력 진행
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine("숫자를 입력해주세요");
+			int key = int.Parse(Console.ReadLine());
+			if(key < 0)
+			{
+				
+			}
+			else 
+			{
+
+			}
+		}
+	}
+}
