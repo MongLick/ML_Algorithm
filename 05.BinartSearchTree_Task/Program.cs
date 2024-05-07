e6df0a03a40a39c067624447a9fa1450134d9eba
MongLick <qkjm4836@gmail.com>
2024-05-07 19:28:26 +0900

    feat : 이진탐색트리

---
+++ 05.BinartSearchTree_Task/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _05.BinartSearchTree_Task
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			// 1번 문제
+			// a : a, b, d, e, c, f, g
+			// b : d, b, e, a, f, c, g
+			// c : d, e, b, f, g, c, a
+
+			// 2번 문제
+			// a : a, b, d, e, f, z, c, g, w
+			// b : d, b, f, e, z, a, c, w, g
+			// c : d, f, z, e, b, w, g, c, a
+
+			// 3번 문제
+			// 4번 문제 
+			// 5번 문제
+
+			// 셋 문제 전부 그림 문제로 대체
+		}
+	}
+}
