274f70e07915728d627a8fa320a5238b23d8d2f4
MongLick <qkjm4836@gmail.com>
2024-05-15 18:38:54 +0900

    feat : 알고리즘 설계기법

---
+++ 08.DesignTechnique_Task/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace _08.DesignTechnique_Task
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			// 1번 문제
+
+			int n = 0, m = 0;
+			string[] temp = Console.ReadLine().Split();
+			n = int.Parse(temp[0]);
+			m = int.Parse(temp[1]);
+			StringBuilder sb = new StringBuilder();
+			Go(new int[m], sb, n, m, 0);
+			Console.WriteLine(sb.ToString());
+
+		}
+
+		static void Go(int[] arr, StringBuilder sb, int n, int m, int c)
+		{
+			if (c == m)
+			{
+				for (int i = 0; i < arr.Length; i++)
+				{
+					sb.Append(arr[i]);
+					sb.Append(' ');
+				}
+				sb.AppendLine();
+				return;
+			}
+
+			for (int i = 0; i < n; i++)
+			{
+				arr[c] = i + 1;
+				Go(arr, sb, n, m, c + 1);
+			}
+		}
+	}
+}
